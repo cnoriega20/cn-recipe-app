11a36a31ee29364181c86da8ce2f589cf3e1c506
cnoriega20 <c79.noriega@gmail.com>
2021-01-16 01:14:23 -0600

    Initial commit: Spring Initializr project.

---
+++ src/test/java/com/springboot/cnrecipeapp/CnRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.cnrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
