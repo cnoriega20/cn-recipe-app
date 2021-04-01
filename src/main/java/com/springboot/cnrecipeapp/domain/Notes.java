45ac70df5a82743856eebca50ebdf16e5f254b48
cnoriega20 <c79.noriega@gmail.com>
2021-04-01 00:58:05 -0500

    Added entities with one-to-one relationship.

---
+++ src/main/java/com/springboot/cnrecipeapp/domain/Notes.java
@@ -0,0 +1,25 @@
+package com.springboot.cnrecipeapp.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+}
---
+++ src/main/java/com/springboot/cnrecipeapp/domain/Recipe.java
@@ -0,0 +1,33 @@
+package com.springboot.cnrecipeapp.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+
+    @Lob
+    private Byte[] image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+
+}
