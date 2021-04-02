9d18d8ddd9d6095769db7343aefa2b2cbe4cc6fa
cnoriega20 <c79.noriega@gmail.com>
2021-04-02 00:48:31 -0500

    Added Many to Many relationship changes.

---
+++ src/main/java/com/springboot/cnrecipeapp/domain/Category.java
@@ -0,0 +1,25 @@
+package com.springboot.cnrecipeapp.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+}
