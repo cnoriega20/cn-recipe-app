ee3af6b9fd6bccedd4c7967e12af8e8933f8e963
cnoriega20 <c79.noriega@gmail.com>
2021-04-03 10:16:34 -0500

    Initialized Sets in Recipe to avoid NPE.

--- src/main/java/com/springboot/cnrecipeapp/domain/Recipe.java
+++ src/main/java/com/springboot/cnrecipeapp/domain/Recipe.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Data
@@ -35,12 +36,12 @@ public class Recipe {
     private Notes notes;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @ManyToMany
     @JoinTable(name = "recipe_category",
             joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
 
 }
