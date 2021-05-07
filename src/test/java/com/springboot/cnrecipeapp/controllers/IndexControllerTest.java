f0e1078060dbefcead0d25f3ac262333519ffe07
cnoriega20 <c79.noriega@gmail.com>
2021-05-06 23:35:45 -0500

    Added ArgumentCaptor to IndexControllerTest.

--- src/test/java/com/springboot/cnrecipeapp/controllers/IndexControllerTest.java
+++ src/test/java/com/springboot/cnrecipeapp/controllers/IndexControllerTest.java
@@ -6,6 +6,7 @@ import com.springboot.cnrecipeapp.repositories.UnitOfMeasureRepository;
 import com.springboot.cnrecipeapp.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
@@ -43,15 +44,29 @@ public class IndexControllerTest {
     @Test
     public void testGetIndexPage() throws Exception{
 
-        Set<Recipe> recipesSet = new HashSet<>();
+        /*********** B D D *********************/
 
+        // Given
+        Set<Recipe> recipesSet = new HashSet<>();
+        recipesSet.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipesSet.add(recipe);
+
+        //When
         when(recipeService.getRecipes()).thenReturn(recipesSet);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+
         String viewModel = indexController.getIndexPage(model);
         assertEquals("index", viewModel);
 
+        //Then
         //Verify mock interactions
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"),anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"),argumentCaptor.capture());
+
+        Set<Recipe> setArgumentCaptor = argumentCaptor.getValue();
+        assertEquals(2, setArgumentCaptor.size());
     }
 
 }
